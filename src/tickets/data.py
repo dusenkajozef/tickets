07aa21825a0a154f054a7c945d29e255380e89a7
dusky_2 <jozef.dusenka.goalie@gmail.com>
2026-03-12 16:53:54 -0500

    initial commit

---
+++ src/tickets/data.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+
+
+def load_data():
+    "https://huggingface.co/datasets/fancyzhx/amazon_polarity"
+    # TODO: write return line here
+    #raise NotImplementedError()
+
+    return load_dataset("fancyzhx/amazon_polarity")
